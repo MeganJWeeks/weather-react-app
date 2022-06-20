ad8b79459559d413d9ebb9554c214567112df789
MeganJWeeks <100442627+MeganJWeeks@users.noreply.github.com>
2022-06-20 21:58:54 +0200

    Add and update footer and git link

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <span class='footer'>
+      <a href='https://github.com/MeganJWeeks/weather-react-app'>
+        Open-source code
+      </a>{" "}
+      by Megan Weeks
+    </span>
+  );
+}
